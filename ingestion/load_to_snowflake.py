37ecc671f36f46e21030bd4d8b00848f8c2e4a9e
jamesdlavin <jdlavin00@gmail.com>
2026-05-18 14:14:01 -0500

    remove 'source_file' column from timeliness data

--- ingestion/load_to_snowflake.py
+++ ingestion/load_to_snowflake.py
@@ -217,6 +217,7 @@ timeliness.columns = (timeliness.columns
 )
 
 timeliness = timeliness[timeliness['region'] != 'TOTAL']
+timeliness = timeliness.drop(columns=['source_file'])
 geographic_regions = ['01', '02/09', '03', '04', '05', '06', '07', '08', '10', '11']
 timeliness['is_geographic_region'] = timeliness['region'].isin(geographic_regions)
 timeliness['loaded_at'] = pd.Timestamp.now()
